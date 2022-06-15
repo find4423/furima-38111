20bea165264b3507dc61842b535cee83230c766a
find4423 <106136699+find4423@users.noreply.github.com>
2022-06-15 16:05:54 +0900

    これより、テストに入ります。コミットのタイミング少し遅いです。

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,18 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  with_options presence: true do
+    validates :nickname
+    validates :birth_day
+  end
+
+    validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/u, allow_blank: true} 
+    validates :last_name , presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/u, allow_blank: true}
+    validates :first_name_kana , presence: true, format: { with: /\A[ァ-ヶー]+\z/u, allow_blank: true}
+    validates :last_name_kana , presence: true, format: { with: /\A[ァ-ヶー]+\z/u, allow_blank: true}
+    # validates :password, length:{minimum:6}, allow_blank: true デフォでかかったやがったこいつ...何の時間やったんや...
+    validates :password, format:{with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/}, allow_blank: true
+
 end
+
+
