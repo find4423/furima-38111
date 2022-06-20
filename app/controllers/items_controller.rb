fd28148659c784bd200843fbe3e0a8d883f8d94a
find4423 <106136699+find4423@users.noreply.github.com>
2022-06-20 11:50:57 +0900

    商品編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,8 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: :new
+
+  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :move_to_index, only: :edit
+
   def index
     @items = Item.includes(:user).order('created_at DESC')
   end
@@ -21,10 +24,30 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path
+    else 
+      render :edit
+    end
+  end
+
   private
 
   def item_params
     params.require(:item).permit(:name, :explanation, :category_id, :condition_id, :shipping_charge_id, :delivery_id, :image,
                                  :pref_id, :price).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    @item = Item.find(params[:id])
+    unless @item.user_id == current_user.id
+      redirect_to root_path
+    end
   end
 end
