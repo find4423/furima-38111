75a1e075730359ad532c67c23939cd3276ab0920
find4423 <106136699+find4423@users.noreply.github.com>
2022-06-22 16:22:28 +0900

    条件分け

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,12 +1,16 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!, ouly: :index
+  before_action :find_item,only: [:index, :create]
+  before_action :move_to_root, only: :index
+
 
   def index
    @order = OrderOrderAddress.new
-   @item = Item.find(params[:item_id])
+  #  @item = Item.find(params[:item_id])
   end
 
   def create
-    @item = Item.find(params[:item_id])
+    # @item = Item.find(params[:item_id])
     @order = OrderOrderAddress.new(order_params)
     if @order.valid?
       Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
@@ -26,5 +30,15 @@ class OrdersController < ApplicationController
 
   def order_params
     params.require(:order_order_address).permit(:post_code, :pref_id, :city, :address_line1, :address_line2, :number).merge(user_id: current_user.id, item_id: @item.id, token: params[:token])
+  end
+
+  def find_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def move_to_root
+    if @item.order.present? || @item.user_id == current_user.id
+      redirect_to root_path
+    end
   end
 end
